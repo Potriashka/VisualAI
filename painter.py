04e4bf6de8c102c914a59d970a552ec9c271a4c7
Peter <54754814+Potriashka@users.noreply.github.com>
2021-03-06 11:26:19 +0200

    Update painter.py

--- painter.py
+++ painter.py
@@ -1,9 +1,27 @@
 import pygame, sys
 from pygame.locals import *
+import requests
+import re
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyAsickaqxGF3TLmQ5Z9YYXm66MbpmM2CD4",
+    "authDomain": "image-storer-948f7.firebaseapp.com",
+    "databaseURL": "https://image-storer-948f7.firebaseio.com",
+    "projectId": "image-storer-948f7",
+    "storageBucket": "image-storer-948f7.appspot.com",
+    "messagingSenderId": "783186859731",
+    "appId": "1:783186859731:web:9a33964ddf11b4fb5e6626",
+    "measurementId": "G-E3NYZ71QLR"
+}
 
 pygame.init()
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
 
 start = False
+
+
 
 FPS = 30 # frames per second setting
 fpsClock = pygame.time.Clock()
@@ -28,11 +46,17 @@ while start: # the main game loop
     DISPLAYSURF.fill(WHITE)
 
     if inp == '1':
-        Img = pygame.image.load('jex.jpg')
+        path_on_cloud = "images/foo.jpg"
+        storage.child(path_on_cloud).download("test_download.jpg")
+        Img = pygame.image.load('test_download.jpg')
 
     elif inp == '2':
-        Img = pygame.image.load('ex2.jpg')
-        img2 = pygame.image.load('jap.png')
+        path_on_cloud = "images/Y6vdnJ.jpg"
+        storage.child(path_on_cloud).download("test_download2.jpg")
+        Img = pygame.image.load('test_download2.jpg')
+        path_on_cloud = "images/cat.jpeg"
+        storage.child(path_on_cloud).download("test_download3.jpg")
+        img2 = pygame.image.load('test_download3.jpg')
         posx2 = 280
         posy2 = 550
 
